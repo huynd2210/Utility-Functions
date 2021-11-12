ed0e5bbc34122fa008e661a65dc659162fa69e56
Duc Huy Nguyen <38105367+huynd2210@users.noreply.github.com>
2021-11-12 14:58:12 +0100

    Update Power Set.java

--- Power Set.java
+++ Power Set.java
@@ -1,17 +1,17 @@
    //It is advised to remove duplicates element from arraylist, as arraylist might contains duplicates
    
-   public static <T> ArrayList powerSet(ArrayList<T> originalSet){
-            ArrayList<ArrayList<T>> powerSet = new ArrayList<>();
-            ArrayList<T> tmp = new ArrayList<>();
-            int powerSetSize = (int)Math.pow(2, originalSet.size());
-            for (int i = 0; i < powerSetSize; i++){
-                for (int j = 0; j < originalSet.size(); j++){
-                    if ((i & (1 << j)) > 0){
-                        tmp.add(originalSet.get(j));
-                    }
+     public static <T> List<List<T>> powerSet(List<T> originalSet){
+        List<List<T>> powerSet = new ArrayList<>();
+        List<T> tmp = new ArrayList<>();
+        int powerSetSize = (int)Math.pow(2, originalSet.size());
+        for (int i = 0; i < powerSetSize; i++){
+            for (int j = 0; j < originalSet.size(); j++){
+                if ((i & (1 << j)) > 0){
+                    tmp.add(originalSet.get(j));
                 }
-                powerSet.add(tmp);
-                tmp = new ArrayList<>();
             }
-            return powerSet;
+            powerSet.add(tmp);
+            tmp = new ArrayList<>();
+        }
+        return powerSet;
     }
